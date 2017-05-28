ef826b6c8a05d7af7c48d490d111bf649f483b1d
efreme <elizaveta.efremova@emc.com>
2017-05-28 19:08:23 +0300

    updates

--- src/main/java/beans/RationalPolynomial.java
+++ src/main/java/beans/RationalPolynomial.java
@@ -29,6 +29,78 @@ public class RationalPolynomial {
         return this;
     }
 
+    public RationalPolynomial add(RationalPolynomial term) {
+        PolynomialFraction currentFractionWithWholePart = new PolynomialFraction()
+                .withNumerator(getWholePart()
+                        .multiply(getReminder().getDenominator())
+                        .add(getReminder().getNumerator()))
+                .withDenominator(getReminder().getDenominator());
+
+        PolynomialFraction termFractionWithWholePart = new PolynomialFraction()
+                .withNumerator(term.getWholePart()
+                        .multiply(term.getReminder().getDenominator())
+                        .add(term.getReminder().getNumerator()))
+                .withDenominator(term.getReminder().getDenominator());
+
+        PolynomialFraction resultedFraction = currentFractionWithWholePart.add(termFractionWithWholePart);
+
+        return resultedFraction.getNumerator().divide(resultedFraction.getDenominator());
+    }
+
+    public RationalPolynomial subtract(RationalPolynomial subtrahend) {
+        PolynomialFraction currentFractionWithWholePart = new PolynomialFraction()
+                .withNumerator(getWholePart()
+                        .multiply(getReminder().getDenominator())
+                        .add(getReminder().getNumerator()))
+                .withDenominator(getReminder().getDenominator());
+
+        PolynomialFraction subtrahendFractionWithWholePart = new PolynomialFraction()
+                .withNumerator(subtrahend.getWholePart()
+                        .multiply(subtrahend.getReminder().getDenominator())
+                        .add(subtrahend.getReminder().getNumerator()))
+                .withDenominator(subtrahend.getReminder().getDenominator());
+
+        PolynomialFraction resultedFraction = currentFractionWithWholePart.subtract(subtrahendFractionWithWholePart);
+
+        return resultedFraction.getNumerator().divide(resultedFraction.getDenominator());
+    }
+
+    public RationalPolynomial multiply(RationalPolynomial multiplier) {
+        PolynomialFraction currentFractionWithWholePart = new PolynomialFraction()
+                .withNumerator(getWholePart()
+                        .multiply(getReminder().getDenominator())
+                        .add(getReminder().getNumerator()))
+                .withDenominator(getReminder().getDenominator());
+
+        PolynomialFraction multiplierFractionWithWholePart = new PolynomialFraction()
+                .withNumerator(multiplier.getWholePart()
+                        .multiply(multiplier.getReminder().getDenominator())
+                        .add(multiplier.getReminder().getNumerator()))
+                .withDenominator(multiplier.getReminder().getDenominator());
+
+        PolynomialFraction resultedFraction = currentFractionWithWholePart.multiply(multiplierFractionWithWholePart);
+
+        return resultedFraction.getNumerator().divide(resultedFraction.getDenominator());
+    }
+
+    public RationalPolynomial divide(RationalPolynomial divisor) {
+        PolynomialFraction currentFractionWithWholePart = new PolynomialFraction()
+                .withNumerator(getWholePart()
+                        .multiply(getReminder().getDenominator())
+                        .add(getReminder().getNumerator()))
+                .withDenominator(getReminder().getDenominator());
+
+        PolynomialFraction divisorFractionWithWholePart = new PolynomialFraction()
+                .withNumerator(divisor.getWholePart()
+                        .multiply(divisor.getReminder().getDenominator())
+                        .add(divisor.getReminder().getNumerator()))
+                .withDenominator(divisor.getReminder().getDenominator());
+
+        PolynomialFraction resultedFraction = currentFractionWithWholePart.divide(divisorFractionWithWholePart);
+
+        return resultedFraction.getNumerator().divide(resultedFraction.getDenominator());
+    }
+
     public PolynomialFraction getReminder() {
         return reminder;
     }
